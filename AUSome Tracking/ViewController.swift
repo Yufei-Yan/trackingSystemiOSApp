193cbefbf1d294e9dca61db68ae262755ea3df9f
Yufei-Yan <yanyufei11111@163.com>
2016-11-30 01:55:52 -0600

    First commit for the tracking system iOS App

---
+++ AUSome Tracking/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AUSome Tracking
+//
+//  Created by Yan on 11/30/16.
+//  Copyright © 2016 Yan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
